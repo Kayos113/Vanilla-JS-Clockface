ff7a91ff1853bd9479f37e4b5b2413ea80f26498
William Knapp <willyknapp@gmail.com>
2022-12-21 12:30:39 -0500

    Initial commit
    This had been a codepen, but I decided to migrate it to github for ease of transferring into other project. It also allowed me to tweak and improve the look slightly from the codepen version.
    
    This is meant to be used as a cutout piece where I will be easily able to grab this clock and place it in other project in varying sizes and looks.

---
+++ index.js
@@ -0,0 +1,27 @@
+let clock = document.getElementById("time");/* $("#time"); */
+let control = document.getElementById("control");/* $("#control"); */
+let onOff = false;
+
+control.onclick = function() {
+  control.classList.toggle("red");
+  clock.classList.toggle("onOff");
+  // setTimeout( () => clock.classList.toggle("onOff"), 500);
+};
+
+control.addEventListener("mousedown", buttonUpDown);
+control.addEventListener("mouseup", buttonUpDown);
+function buttonUpDown() {
+  control.classList.toggle("pressed");
+}
+
+setInterval( () => setTime(), 200);
+function setTime() {
+  let currTime = Date.now();
+  let date = new Date(currTime);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+
+  let string = (hours===12?12:(hours%12<10?"0"+hours%12:hours%12)) + ":" +(minutes<10?"0"+minutes:minutes) + ":" + (seconds<10?"0"+seconds:seconds) + (hours>=12?" PM":" AM");
+  clock.innerHTML = string;
+}
